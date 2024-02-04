41d535f907b604f69afbc463694225c97f98677f
admin <admin@DESKTOP-FLAOSQM>
2024-02-04 20:33:29 +0530

    Save changes

--- src/test/java/actionClassDiscussion/ActionsClassMethodsDiscussion.java
+++ src/test/java/actionClassDiscussion/ActionsClassMethodsDiscussion.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class ActionsClassMethodsDiscussion {
+	//Action class discussion
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
